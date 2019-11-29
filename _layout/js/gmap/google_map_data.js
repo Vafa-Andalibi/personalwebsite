023d2a4f9887f6769a6788690675c72c452b32dd
Vafa Andalibi <vafa.andalibi@gmail.com>
2019-11-29 17:14:57 -0500

    minor updatE

--- _layout/js/gmap/google_map_data.js
+++ _layout/js/gmap/google_map_data.js
@@ -60,7 +60,7 @@ var skillset = {
         'Programming/Scripting Languages': ['Python'],
         'Tools/Frameworks': ['Git', 'Pycharm'],
         'OS': ['Windows', 'Linux: Ubuntu', 'MacOS'],
-        'Other Skills': ['DevOps'],
+        'Other Skills': ['DevOps', 'CI'],
         'description': `I had the opportunity to contribute to Cloudmesh project. 
                         Cloudmesh enables you to access multi-cloud environments such as AWS, Azure, Google, and OpenStack Clouds very easily. 
                         The repository that I was mostly contributed to is available in <a href="https://github.com/cloudmesh/cloudmesh-cloud" target="_blank">GitHub</a>.
